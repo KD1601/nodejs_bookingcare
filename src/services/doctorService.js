c8a158e3637c1d7f6e8748bb69a69572b8ecaf1e
Khanh Duy <khanhduy8768@gmail.com>
2023-10-13 21:53:10 +0700

    Done video 74 BulkCreate with Sequelize, compare data with db using lodash

--- src/services/doctorService.js
+++ src/services/doctorService.js
@@ -1,5 +1,9 @@
 const db = require("../models")
+require('dotenv').config()
+import _ from 'lodash';
 
+
+const MAX_NUMBER_SCHEDULE = process.env.MAX_NUMBER_SCHEDULE
 
 let getTopDoctorHome = (limit) => {
     return new Promise(async (resolve, reject) => {
@@ -137,6 +141,56 @@ let getDetailDoctorById = (id) => {
     })
 }
 
+let bulkCreateSchedule = (data) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            if (!data.arrSchedule || !data.doctorId || !data.formattedDate) {
+                resolve({
+                    errCode: 1,
+                    errMessage: 'Missing required parameter'
+                })
+            } else {
+                let schedule = data.arrSchedule
+                if (schedule && schedule.length > 0) {
+                    schedule = schedule.map(item => {
+                        item.maxNumber = MAX_NUMBER_SCHEDULE
+                        return item
+                    })
+                }
+                // Get all existing data
+                let existing = await db.Schedule.findAll({
+                    where: { doctorId: data.doctorId, date: data.formattedDate },
+                    attributes: ['timeType', 'date', 'doctorId', 'maxNumber'],
+                    raw: true
+                })
+                // Convert date
+                if (existing && existing.length > 0) {
+                    existing = existing.map(item => {
+                        item.date = new Date(item.date).getTime()
+                        return item
+                    })
+                }
+                // Compare different 
+                let toCreate = _.differenceWith(schedule, existing, (a, b) => {
+                    return a.timeType === b.timeType && a.date === b.date
+                })
+                // Create data
+                if (toCreate && toCreate.length > 0) {
+                    await db.Schedule.bulkCreate(toCreate)
+                }
+
+                resolve({
+                    errCode: 0,
+                    errMessage: 'OK'
+                })
+            }
+        } catch (e) {
+            reject(e)
+        }
+    })
+}
+
 module.exports = {
-    getTopDoctorHome, getAllDoctors, getInfoDoctor, getDetailDoctorById
+    getTopDoctorHome, getAllDoctors, getInfoDoctor, getDetailDoctorById,
+    bulkCreateSchedule
 }
